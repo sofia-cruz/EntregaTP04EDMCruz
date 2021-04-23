fe5e7f16725e779e74bbbbee7bcfeb2fc02da85e
Sofia Cruz <sofiacruz@example>
2021-04-23 18:08:43 -0300

    Inicialización de Proyecto
    Cruz, Sofía Candela

---
+++ src/main/java/ar/edu/unju/edm/service/imp/ProductoServiceImp.java
@@ -0,0 +1,71 @@
+package ar.edu.unju.edm.service.imp;
+
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.edm.model.Producto;
+import ar.edu.unju.edm.service.IProductoService;
+
+@Service
+public class ProductoServiceImp implements IProductoService{
+	
+	private static final Log LOGGER = LogFactory.getLog(ProductoServiceImp.class);
+	
+	@Autowired
+	Producto unProducto;
+
+	ArrayList<Producto> listaDeProductos = new ArrayList<Producto>();
+
+	@Override
+	public void guardarProducto(Producto unProducto) {
+		// TODO Auto-generated method stub
+		System.out.println(unProducto.getNombre());
+		listaDeProductos.add(unProducto);
+		System.out.println(listaDeProductos.size());
+		
+		LOGGER.info("METHOD: ingresando a Guardar Producto");
+		LOGGER.info("RESULT: guardado " + listaDeProductos.get(listaDeProductos.size()-1).getNombre());
+	}
+
+	@Override
+	public void modificarProducto(Producto productoAModificar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarProducto(Producto productoAEliminar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Producto obtenerUnProducto(String nombreProducto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Producto obtenerProductoNuevo() {
+		// TODO Auto-generated method stub
+		return unProducto;
+	}
+
+	@Override
+	public ArrayList<Producto> obtenerTodosProductos() {
+		// TODO Auto-generated method stub
+		return listaDeProductos;
+	}
+
+	@Override
+	public Producto obtenerUltimoProducto() {
+		// TODO Auto-generated method stub
+		int i = listaDeProductos.size() - 1;
+		return listaDeProductos.get(i);
+	}
+	
+}
